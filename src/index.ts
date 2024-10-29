b7404d7bbfd804f3ae436cffda2f58992d111a31
razvanbaboiucs <razvanbaboiu@gmail.com>
2024-10-29 21:35:42 +0200

    setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import {cors} from 'hono/cors'
 
 const app = new Hono()
+
+app.use("/*", cors({
+  origin: "https://dekra-forms.pages.dev",
+}))
 
 app.get('/', (c) => {
   return c.text('Hello from the other side!')
